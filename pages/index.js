66653f07526c64fa632a22d7015ed53efa01287f
Diego Munoz <diego.munoz@kushkipagos.com>
2022-12-08 15:48:11 -0500

    Displaying Events

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
+import Link from "next/link";
 
 export default function HomePage({ events }) {
-
     return (
         <Layout>
             <h1>Upcoming Events</h1>
+            {events.length === 0 && <h3>No events to show </h3>}
+            {events.map((evt) => (
+                <EventItem key={evt.id} evt={evt} />
+            ))}
+            {events.length > 0 && (
+                <Link href="/events">
+                    <a className="btn-secondary">View All Events</a>
+                </Link>
+            )}
         </Layout>
     );
 }
@@ -15,7 +25,7 @@ export async function getStaticProps() {
     const events = await res.json();
 
     return {
-        props: { events },
-        revalidate: 1
+        props: { events: events.slice(0, 3) },
+        revalidate: 1,
     };
 }
