3e8458ccfd5e049fcece8631991fbf01eaea3412
Diego Munoz <diego.munoz@kushkipagos.com>
2022-12-27 15:26:36 -0500

    Store JWT in server httpOnly Cookie

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -186,9 +186,11 @@ export default function EditEventPage({ evt }) {
     );
 }
 
-export async function getServerSideProps({ params: { id } }) {
+export async function getServerSideProps({ params: { id }, req }) {
     const res = await fetch(`${API_URL}/api/events/${id}?populate=*`);
     const evt = await res.json();
+
+    // req.headers.cookie
 
     return {
         props: {
