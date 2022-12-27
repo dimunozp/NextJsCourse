8d431cb55294912191f9b330ec2c0350e89b5717
Diego Munoz <diego.munoz@kushkipagos.com>
2022-12-27 13:05:18 -0500

    Auth Context

--- pages/account/register.js
+++ pages/account/register.js
@@ -5,6 +5,7 @@ import { useState, useEffect, useContext } from "react";
 import Link from "next/link";
 import Layout from "@/components/Layout";
 import styles from "@/styles/AuthForm.module.css";
+import AuthContext from "@/context/AuthContext";
 
 export default function RegisterPage() {
     const [username, setUsername] = useState("");
@@ -12,15 +13,17 @@ export default function RegisterPage() {
     const [password, setPassword] = useState("");
     const [passwordConfirm, setPasswordConfirm] = useState("");
 
+    const { register, error } = useContext(AuthContext);
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if(password !== passwordConfirm){
-            toast.error("Password do not match!")
-            return
+        if (password !== passwordConfirm) {
+            toast.error("Password do not match!");
+            return;
         }
 
-        
+        register({ username, email, password });
     };
 
     return (
